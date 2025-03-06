e287f57eadd38bcc28accea8f40123a6649c81f9
Rylan Pow <powrylan@gmail.com>
2025-03-06 05:40:58 -0800

    finished alu.c, untested

--- alu.c
+++ alu.c
@@ -16,3 +16,48 @@ static uint32_t adder_32bit(uint32_t a, uint32_t b) {
 
     return sum;
 }
+
+uint32_t alu_execute(uint32_t a, uint32_t b, ALUOp f, uint32_t *zero) {
+    uint32_t y = 0;
+
+    switch(f){
+        case ALU_AND:
+            y = a & b;
+            break;
+        
+        case ALU_OR:
+            y = a | b;
+            break;
+        
+        case ALU_ADD:
+            y = adder_32bit(a, b);
+            break;
+        
+        case ALU_AND_NOT:
+            y = a & ~b;
+            break;
+        
+        case ALU_OR_NOT:
+            y = a | ~b;
+            break;
+        
+        case ALU_SUB:
+            // two's complement: a - b = a + (-b) + 1
+            y = adder_32bit(a, ~b);
+            y = adder_32bit(y, 1);
+            break;
+
+        case ALU_SLT:
+            y = adder_32bit(a, ~b);
+            y = adder_32bit(y, 1);
+            y = (y >> 31) & 1;
+            break;
+        
+        default:
+            y = 0;
+            break;
+    }
+
+    *zero = (y == 0); //necessary for branching
+    return y;    
+}
